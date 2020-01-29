4b283e950fa661690360ce8ba35c1778e5859dd2
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-29 15:32:33 +1100

    added test case

--- LCS.Test/Test.cs
+++ LCS.Test/Test.cs
@@ -17,6 +17,9 @@ namespace LCS.Test
         public void LCS_of_1_empty_string_is_empty() => Compute("ABCD", "").Should().BeEmpty();
 
         [Test]
+        public void LCS_of_10010101_and_BDCABA_is_BCBA() => Compute("10010101", "010110110").Should().Be("100110");
+
+        [Test]
         public void LCS_of_ABCBDAB_and_BDCABA_is_BCBA() => Compute("ABCBDAB", "BDCABA").Should().Be("BCBA");
 
         [Test]
